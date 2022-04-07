6357f21c12fbd9991b7f872c296ddd3a7c3dfc6e
Daniel Carrizo <daniel.german.carrizo@gmail.com>
2022-04-07 14:05:34 -0300

    Entorno levantado ok

---
+++ entrega1carrizo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from entrega1carrizo.users.models import User
+from entrega1carrizo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
